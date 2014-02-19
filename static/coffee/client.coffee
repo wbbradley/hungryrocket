26583cf93b3c21a90b2a5456884dc98585f39617
William Bradley <williambbradley@gmail.com>
2014-02-18 21:45:56 -0800

    boilerplate client-server setup

--- static/coffee/client.coffee
+++ static/coffee/client.coffee
@@ -1,5 +1,30 @@
-login = ->
-  window.alert 'asdfasdf'
+class Server
+  constructor: (props) ->
+    @props = props
 
-@.Rocket = {}
-@.Rocket.login = login
+  connect: (host) =>
+    console.log "Connecting..."
+
+    fake_props =
+      motd: 'Welcome'
+    window.setTimeout((=> @connected(fake_props)), 0)
+
+  connected: (server_props) =>
+    console.log "Server connected"
+    console.dir server_props
+
+  login: =>
+    console.log $('#username').val()
+    server.set_name $('#username').val()
+
+  set_name: =>
+    # TODO(mrjaeger): call the server
+
+  update_game_state: (game_state) =>
+    console.dir game_state
+
+server = new Server()
+server.connect()
+
+@Rocket = {}
+@Rocket.login = server.login
--- static/js/client.js
+++ static/js/client.js
@@ -1,12 +1,56 @@
 (function() {
-  var login;
+  var Server, server,
+    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
-  login = function() {
-    return window.alert('asdfasdf');
-  };
+  Server = (function() {
+    function Server(props) {
+      this.update_game_state = __bind(this.update_game_state, this);
+      this.set_name = __bind(this.set_name, this);
+      this.login = __bind(this.login, this);
+      this.connected = __bind(this.connected, this);
+      this.connect = __bind(this.connect, this);
+      this.props = props;
+    }
+
+    Server.prototype.connect = function(host) {
+      var fake_props;
+      console.log("Connecting...");
+      fake_props = {
+        motd: 'Welcome'
+      };
+      return window.setTimeout(((function(_this) {
+        return function() {
+          return _this.connected(fake_props);
+        };
+      })(this)), 0);
+    };
+
+    Server.prototype.connected = function(server_props) {
+      console.log("Server connected");
+      return console.dir(server_props);
+    };
+
+    Server.prototype.login = function() {
+      console.log($('#username').val());
+      return server.set_name($('#username').val());
+    };
+
+    Server.prototype.set_name = function() {};
+
+    Server.prototype.update_game_state = function(game_state) {
+      return console.dir(game_state);
+    };
+
+    return Server;
+
+  })();
+
+  server = new Server();
+
+  server.connect();
 
   this.Rocket = {};
 
-  this.Rocket.login = login;
+  this.Rocket.login = server.login;
 
 }).call(this);
